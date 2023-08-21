636f938aba869e05fefe16d3006366a8e40f8e11
BuildTools <unconfigured@null.spigotmc.org>
2023-08-21 21:50:57 +0300

    Initial commit

---
+++ src/main/java/ua/lich/coding/hideplayerkiller/DeathListener.java
@@ -0,0 +1,40 @@
+package ua.lich.coding.hideplayerkiller;
+
+import org.bukkit.Bukkit;
+
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.potion.PotionEffectType;
+
+public class DeathListener implements Listener {
+
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+
+        try {
+            if(event.getEntity().getKiller() != null && event.getEntity().getKiller().getType() == EntityType.PLAYER) {
+                //Who killed
+                Player player = event.getEntity();
+
+                //Killer
+                Player killer = event.getEntity().getKiller();
+
+
+                //Check Invisible
+                if (killer != null && killer.hasPotionEffect(PotionEffectType.INVISIBILITY)) {
+                    event.setDeathMessage(player.getName() + " was killed.");
+                }
+
+            }
+        } catch (NullPointerException e) {
+            Bukkit.getServer().getConsoleSender().sendMessage("Игрок просто умер.");
+        }
+    }
+
+}
---
+++ src/main/java/ua/lich/coding/hideplayerkiller/Plugin.java
@@ -0,0 +1,13 @@
+package ua.lich.coding.hideplayerkiller;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Plugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getServer().getPluginManager().registerEvents(new DeathListener(), this);
+    }
+}
